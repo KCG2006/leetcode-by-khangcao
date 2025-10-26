e2b7c9ce2dd607f2acdc51be4d28e59224ec9efd
Khang <khangcao0000@gmail.com>
2025-10-26 12:26:25 -0400

    Add initial solution

---
+++ easy/100_same_tree.py
@@ -0,0 +1,32 @@
+# my solution isn't fast because it computes every element for each tree then compares them
+# better way: compare each element of both tree as you traverse through them
+
+# Time: O(N+M)
+# Space: O(N+M)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        elements_1 = []
+        elements_2 = []
+        self.preOrder(p, elements_1)
+        self.preOrder(q, elements_2)
+        return (elements_1 == elements_2)
+
+    def preOrder(self, tree, elements):
+        """should add to elements using preOrder traversal"""
+        if tree is not None:
+            elements.append(tree.val)
+            elements.append(self.preOrder(tree.left, elements))
+            elements.append(self.preOrder(tree.right, elements))
+
