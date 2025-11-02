0ca710ad3b359d560fd7405f9e9dc525bb7c929c
Khang <khangcao0000@gmail.com>
2025-11-01 23:24:38 -0400

    add solution2 035_search_insert_position

--- easy/035_search_insert_position.py
+++ easy/035_search_insert_position.py
@@ -22,4 +22,26 @@ class Solution(object):
 
         return binary_search(0, len(nums) - 1)
 
+#Time: O(log N)
+# Space: O(1)
+class Solution2(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
 
