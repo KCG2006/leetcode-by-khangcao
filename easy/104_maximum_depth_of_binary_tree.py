8b4a79f5e1f9cd1e16b5f61cf29683749c563293
Khang <khangcao0000@gmail.com>
2025-11-07 17:53:01 -0500

    Add solution2 104

--- easy/104_maximum_depth_of_binary_tree.py
+++ easy/104_maximum_depth_of_binary_tree.py
@@ -16,3 +16,36 @@ class Solution(object):
         if root is None:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+# Time: O(N)
+# Space: O(N)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        depth = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            depth += 1
+        return depth
+
+
