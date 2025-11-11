3a0a09b9f27e76ce3efbb03edb2a797951ff299e
Khang <khangcao0000@gmail.com>
2025-11-10 21:10:15 -0500

    add solution2 to 101

--- easy/101_symmetric_tree.py
+++ easy/101_symmetric_tree.py
@@ -23,3 +23,37 @@ class Solution(object):
             else:
                 return check_symmetric(left_child.left, right_child.right) and check_symmetric(left_child.right, right_child.left)
         return check_symmetric(root.left, root.right)
+
+# Time: O(N)
+# Space: O(N)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2(object):
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return True
+
+        q = deque()
+        q.append((root.left, root.right))
+
+        while q:
+            left, right = q.popleft()
+            if not left and not right:
+                continue
+            if (not left and right) or (left and not right):
+                return False
+            if left.val != right.val:
+                return False
+            else:
+                q.append((left.left, right.right))
+                q.append((left.right, right.left))
+        return True
