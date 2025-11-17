d905c329e5134cce30f83ffc6e3cfb29d3928223
Khang <khangcao0000@gmail.com>
2025-11-17 17:57:53 -0500

    modify 026 solution

--- easy/026_remove_duplicates_from_sorted_array.py
+++ easy/026_remove_duplicates_from_sorted_array.py
@@ -7,9 +7,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
         count = 1
         for i in range(1, len(nums)):
-            if nums[i] != nums[count-1]:
-                nums[count] = nums[i]
-                count+=1
+            if nums[i] != nums[count - 1]:
+                count += 1
+                nums[count - 1] = nums[i]
         return count
--- easy/169_majority_element.py
+++ easy/169_majority_element.py
@@ -13,3 +13,20 @@ class Solution(object):
                 dct[nums[i]] = 1
             if dct[nums[i]] > (len(nums) // 2):
                 return nums[i]
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        major = 0
+        res = None
+        for i in nums:
+            if major == 0:
+                res = i
+            major += 1 if i == res else -1
+        return res
