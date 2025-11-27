7f4c9f4f9c6f0b203e847c77b01a9b15d49f7f95
Khang <khangcao0000@gmail.com>
2025-11-27 10:55:34 -0500

    add solution 66: plus one

---
+++ easy/066_plus_one.py
@@ -0,0 +1,18 @@
+# Time: O(N)
+# Space: O(1)
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        i = len(digits) - 1
+        while i >= 0 and digits[i] == 9:
+            digits[i] = 0
+            i -= 1
+        if i >= 0:
+            digits[i] += 1
+        else:
+            return [1] + digits
+        return digits
