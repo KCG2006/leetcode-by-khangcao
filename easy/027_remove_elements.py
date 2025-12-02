ff893c06c214ed93ba2364bafe738b57cba0e06b
Khang <khangcao0000@gmail.com>
2025-12-02 12:57:09 -0500

    Add solution2 to leetcode 27

--- easy/027_remove_elements.py
+++ easy/027_remove_elements.py
@@ -14,3 +14,24 @@ class Solution(object):
                 nums[count] = nums[i]
                 count += 1
         return len(nums[:count])
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution2(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+        while left < right:
+            if nums[left] == val:
+                nums[left] = nums[right - 1]
+                right -= 1
+            else:
+                left += 1
+        return right
+
+
