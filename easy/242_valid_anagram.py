001bab5434a9d2b345c52d87216de4f90dfb59d0
Khang <khangcao0000@gmail.com>
2025-12-04 11:21:04 -0500

    Add solution to leetcode 242

---
+++ easy/242_valid_anagram.py
@@ -0,0 +1,18 @@
+# Time: O(N)
+# Space: O(1)
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        dict_s = {}
+        dict_t = {}
+        for i in range(len(s)):
+            dict_s[s[i]] = dict_s.get(s[i], 0) + 1
+            dict_t[t[i]] = dict_t.get(t[i], 0) + 1
+        return dict_s == dict_t
