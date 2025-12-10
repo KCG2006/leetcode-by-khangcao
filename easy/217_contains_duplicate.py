67411cee52d8a71e5e64bed76f0b063f3254fb35
Khang <khangcao0000@gmail.com>
2025-12-10 13:01:50 -0500

    add solution 217

---
+++ easy/217_contains_duplicate.py
@@ -0,0 +1,15 @@
+# Time: O(N)
+# Space: O(N)
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums_set = set()
+        for i in range(len(nums)):
+            if nums[i] in nums_set:
+                return True
+            nums_set.add(nums[i])
+        return False
