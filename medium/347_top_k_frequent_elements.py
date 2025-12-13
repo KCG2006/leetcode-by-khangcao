b145fc238662f6b8729ea3754a2712c40bbe1213
Khang <khangcao0000@gmail.com>
2025-12-13 11:53:30 -0500

    add solution 2 and 3 to 347

--- medium/347_top_k_frequent_elements.py
+++ medium/347_top_k_frequent_elements.py
@@ -16,3 +16,55 @@ class Solution(object):
         return sorted_count[:k]
 
 
+# Time: O(N*logK)
+# Space: O(N)
+import heapq
+from collections import Counter
+class Solution2(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counter = Counter(nums)
+        heap = []
+
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+
+        return [h[1] for h in heap]
+
+
+# Time: O(N)
+# Space: O(N)
+from collections import Counter
+class Solution3(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        counter = Counter(nums)
+        bucket = [0]*(n+1) #because the freq never go beyond n
+        for num, freq in counter.items():
+            if bucket[freq] == 0:
+                bucket[freq] = [num]
+            else:
+                bucket[freq].append(num)
+        ret = []
+        for i in range(n, -1, -1):
+            if bucket[i] != 0:
+                ret.extend(bucket[i])
+            if len(ret) == k:
+                return ret
+
+
+
+
+
